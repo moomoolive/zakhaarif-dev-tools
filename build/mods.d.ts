61ade9d5016695df2ab3d108f6773d80511fe431
moomoo <mostafaelbannan1@gmail.com>
2023-03-13 15:58:08 -0700

    changed the way shaheen and mods are extended

--- build/mods.d.ts
+++ build/mods.d.ts
@@ -1,11 +1,15 @@
 export type ShaheenCore = {
     getRootCanvas: () => HTMLCanvasElement;
 };
-export type ShaheenEngine<Extensions extends object = object> = (ShaheenCore & Extensions);
-export type ZakhaarifModCore<EngineExtensions extends object = object> = {
-    init: (engine: ShaheenEngine<EngineExtensions>) => unknown;
+export interface ShaheenEngineExtensions {
+}
+export type ShaheenEngine = ShaheenCore & ShaheenEngineExtensions;
+export type ZakhaarifModCore = {
+    init: (engine: ShaheenEngine) => unknown;
 };
-export type ZakhaarifMod<EngineExtensions extends object = object, ModExtensions extends object = object> = (ZakhaarifModCore<EngineExtensions> & ModExtensions);
-export type ZakhaarifModEsModule<EngineExtensions extends object = object, ModExtensions extends object = object> = {
-    default: ZakhaarifMod<EngineExtensions, ModExtensions>;
+export interface ZakhaarifModExtensions {
+}
+export type ZakhaarifMod = (ZakhaarifModCore & ZakhaarifModExtensions);
+export type ZakhaarifModEsModule = {
+    default: ZakhaarifMod;
 };
--- src/mods.ts
+++ src/mods.ts
@@ -2,25 +2,21 @@ export type ShaheenCore = {
     getRootCanvas: () => HTMLCanvasElement
 }
 
-export type ShaheenEngine<Extensions extends object = object> = (
-    ShaheenCore & Extensions
-)
+export interface ShaheenEngineExtensions {}
 
-export type ZakhaarifModCore<EngineExtensions extends object = object> = {
-    init: (engine: ShaheenEngine<EngineExtensions>) => unknown
+export type ShaheenEngine = ShaheenCore & ShaheenEngineExtensions
+
+export type ZakhaarifModCore = {
+    init: (engine: ShaheenEngine) => unknown
 }
 
-export type ZakhaarifMod<
-    EngineExtensions extends object = object,
-    ModExtensions extends object = object
-> = (
-    ZakhaarifModCore<EngineExtensions>
-    & ModExtensions
+export interface ZakhaarifModExtensions {}
+
+export type ZakhaarifMod = (
+    ZakhaarifModCore
+    & ZakhaarifModExtensions
 )
 
-export type ZakhaarifModEsModule<
-    EngineExtensions extends object = object,
-    ModExtensions extends object = object
-> = {
-    default: ZakhaarifMod<EngineExtensions, ModExtensions>
+export type ZakhaarifModEsModule = {
+    default: ZakhaarifMod
 }
