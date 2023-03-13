c0f45c75e510b8709485504e202530fe47070aa0
moomoo <mostafaelbannan1@gmail.com>
2023-03-13 14:57:41 -0700

    started to work on mod format

--- src/mods.ts
+++ src/mods.ts
@@ -1,7 +1,26 @@
-export type ZakhaarifMod = {
-    init: Function
+export type ShaheenCore = {
+    getRootCanvas: () => HTMLCanvasElement
 }
 
-export type ZakhaarifModEsModule = {
-    default: ZakhaarifMod
+export type ShaheenEngine<Extensions extends object = object> = (
+    ShaheenCore & Extensions
+)
+
+export type ZakhaarifModCore<EngineExtensions extends object = object> = {
+    init: (engine: ShaheenEngine<EngineExtensions>) => unknown
+}
+
+export type ZakhaarifMod<
+    EngineExtensions extends object = object,
+    ModExtensions extends object = object
+> = (
+    ZakhaarifModCore<EngineExtensions>
+    & ModExtensions
+)
+
+export type ZakhaarifModEsModule<
+    EngineExtensions extends object = object,
+    ModExtensions extends object = object
+> = {
+    default: ZakhaarifMod<EngineExtensions, ModExtensions>
 }
